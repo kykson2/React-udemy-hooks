d395fc6ff19374941e0acc1d8019dc4f56e42782
GWANWOO <ganwoo98@gmail.com>
2021-07-19 02:24:48 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordian from "./component/Accordion";
+import Search from "./component/Search";
+import Dropdown from "./component/Dropdown";
 
-function App() {
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
+
+const options = [
+  {
+    label: "The color Red",
+    value: "red",
+  },
+  {
+    label: "The color Green",
+    value: "green",
+  },
+  {
+    label: "A shade of Blue",
+    value: "blue",
+  },
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Dropdown options={options} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/component/Search.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Search = () => {
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeOutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 500);
+
+      // 기존의 setTimeout 시간을 검색어가 수정 중일 때
+      // 계속 해서 초기화 시킴으로 써 검색어가 완성되기 전까지 검색 결과 출력 안함
+      return () => {
+        clearTimeout(timeOutId);
+      };
+    }
+  }, [term, results.length]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            className="input"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="ui celled list">{renderedResults}</div>
+    </div>
+  );
+};
+
+export default Search;
